d0e5190aa879fa6d2e0cb4ae00bd7fb149c9b038
keeganasmith <keeganasmith2003@gmail.com>
2025-03-20 21:50:34 -0500

    working

--- project/main.cpp
+++ project/main.cpp
@@ -2,10 +2,11 @@
 //n ∈ {9, 10}, k ∈ {4, 5, 6}, m ∈ {2, 3, · · · , n − k}
 using std::cout;
 int main(){
-    int n = 9;
-    int k = 4;
-    int m = 2;
-    vector<vector<double>> G = generate_random_g(n, k, m);
-    double result = solve(k, n, G, m);
-    cout << result << "\n";
+    for(int i = 0; i < 5; i++){
+      int n = rand() % 101;
+      int k = rand() % 3 + 4;
+      int m = rand() % (n - k - 2 + 1) + 2;
+      vector<vector<double>> G = generate_random_g(n, k, m);
+      double result = solve(k, n, G, m);
+    }
 }
--- project/test.cpp
+++ project/test.cpp
@@ -5,5 +5,6 @@ int main(){
     vector<double> row2{0, 1, -0.8960798, -0.7442706, 0.3345122};
     vector<vector<double>> G{row1, row2};
     double result = solve(2, 5, G, 2);
-    cout << result;
+    cout << "got here\n";
+    cout << result << "\n";
 }
