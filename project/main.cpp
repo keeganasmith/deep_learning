56d3d397b1224399397235a8d35549540d23402e
keeganasmith <keeganasmith2003@gmail.com>
2025-03-21 01:24:08 -0500

    fine

--- project/main.cpp
+++ project/main.cpp
@@ -53,7 +53,7 @@ int main(int argc, char** argv) {
     srand(time(NULL) + rank); 
     // Each node generates 5 results.
     vector<Result> local_results;
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < 2000; i++){
         int n = rand() % 2 + 9;            // n in {9, 10}
         int k = rand() % 3 + 4;              // k in {4, 5, 6}
         int m = rand() % (n - k - 2 + 1) + 2;  // m in [2, n-k]
@@ -68,29 +68,28 @@ int main(int argc, char** argv) {
         vector<char> ser = r.serialize();
         localBuffer.insert(localBuffer.end(), ser.begin(), ser.end());
     }
-    int localSize = localBuffer.size();
-    
-    // Compute each process's offset using MPI_Exscan.
-    int local_offset = 0;
-    MPI_Exscan(&localSize, &local_offset, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
-    if (rank == 0)
-        local_offset = 0; // rank 0 starts at 0.
-    
-    // Open the binary file in read/write mode without truncating existing content.
     MPI_File fh;
-    MPI_File_open(MPI_COMM_WORLD, "results.bin", MPI_MODE_CREATE | MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
-    
-    // Get the current size of the file (if any data exists already).
+    MPI_File_open(MPI_COMM_WORLD, "results.bin",
+                  MPI_MODE_CREATE | MPI_MODE_RDWR,
+                  MPI_INFO_NULL, &fh);
+
+    // Use MPI_Offset for file size and offsets.
     MPI_Offset file_size;
     MPI_File_get_size(fh, &file_size);
-    
-    // Compute the overall offset for this process: pre-existing file size + offset from lower ranks.
+
+    MPI_Offset localSize = localBuffer.size();  // localBuffer is a vector<char>
+    MPI_Offset local_offset = 0;
+    MPI_Exscan(&localSize, &local_offset, 1, MPI_OFFSET, MPI_SUM, MPI_COMM_WORLD);
+    if (rank == 0){
+      local_offset = 0;
+    }
+    // Overall offset is the current file size plus the computed local offset.
     MPI_Offset my_offset = file_size + local_offset;
-    
-    // Write the binary buffer at the computed offset.
-    MPI_File_write_at(fh, my_offset, localBuffer.data(), localSize, MPI_CHAR, MPI_STATUS_IGNORE);
-    
+
+    // Use collective write.
+    MPI_File_write_at_all(fh, my_offset, localBuffer.data(), localSize, MPI_CHAR, MPI_STATUS_IGNORE);
+
     MPI_File_close(&fh);
-    MPI_Finalize();
+
     return 0;
 }
