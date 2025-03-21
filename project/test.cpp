ab82fe961478c097b880b02067678a4bdf1d6215
Keegan Smith <keeganasmith2003@gmail.com>
2025-03-20 21:39:51 -0500

    test

---
+++ project/test.cpp
@@ -0,0 +1,9 @@
+#include <data_generation.h>
+using std::vector, std::cout;
+int main(){
+    vector<double> row1{1, 0, .4759809, .9938236, .819425};
+    vector<double> row2{0, 1, -0.8960798, -0.7442706, 0.3345122};
+    vector<vector<double>> G{row1, row2};
+    double result = solve(2, 5, G, 2);
+    cout << result;
+}
